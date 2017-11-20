67e33f28a6e097d4a639473a731196467fc2cf87
Le Vu Hoang <rainyhoang@gmail.com>
2017-11-20 18:41:19 -0500

    Add on

---
+++ app/data/friends.js
@@ -0,0 +1,27 @@
+var friends = [
+	{
+	"name" : "LEON",
+	"photo" : "http://media.missteen.vn/images/16082017184720_IMG_1223v.jpg",
+	"scores" : [5,5,2,3,1,5,2,1,3,5]
+},
+{
+	"name" : "Clair",
+	"photo" : "http://streaming1.danviet.vn/upload/3-2016/images/2016-07-01/146736335339183-ph---ph----ng-anh--2-.jpg",
+	"scores" : [1,5,1,1,1,1,1,1,5,5]
+},
+{
+	"name" : "Leonie",
+	"photo" : "https://www.dentaltownsmiles.com/wp-content/uploads/2015/03/team-vi-300x300.jpg",
+	"scores" : [5,3,2,3,2,5,1,3,3,4]
+},
+{
+	"name" : "Vi Ho",
+	"photo" : "https://s3.amazonaws.com/aphs.worldnomads.com/vivi/27265/Picture_090_1.jpg",
+	"scores" : [1,3,5,2,4,1,1,2,2,4]
+}
+
+
+];
+
+
+module.exports = friends;
